0c4d4a5b2c02053d7afaacb81ecb2ebe71c8cc77
aghp81 <aghp81@gmail.com>
2024-06-05 16:33:03 +0330

    req, res, port 3000, send API

--- index.js
+++ index.js
@@ -5,4 +5,9 @@ const app = express()
 
 app.listen(3000, ()=>{
     console.log('server is running on port 3000');
-})
+});
+
+
+app.get('/', (req, res)=> {
+    res.send("Hello from node API");
+});
