6d012f1b1603ff4b177bfebacd964b8912c9342a
aghp81 <aghp81@gmail.com>
2024-06-07 13:55:18 +0330

    POST data to database

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const mongoose = require("mongoose");
-const Product = require('./models/product.model.js')
+const Product = require('./models/product.model.js');
 const app = express();
 
 // built-in middleware function in Express parses incoming requests with JSON این باعث میشه که API کار کنه
@@ -12,9 +12,10 @@ app.get("/", (req, res) => {
   res.send("Hello from node API Server");
 });
 
-app.post("/api/products", (req, res) => {
+app.post("/api/products", async (req, res) => {
   try{
-
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
   }catch(error){
     res.status(500).json({message: error.message});
   }
