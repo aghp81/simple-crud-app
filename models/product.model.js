df280b1f8162afe9d3a4cbc44b9c99a6681e72e7
aghp81 <aghp81@gmail.com>
2024-06-07 11:07:42 +0330

    ساخت مدل برای محصولات در دیتابیس

--- models/product.model.js
+++ models/product.model.js
@@ -1,3 +1,31 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const ProductSchema = mongoose.Schema({});
+const ProductSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "لطفا نام محصول را وارد کنید."],
+    },
+
+    quantity: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+
+    price: {
+      type: Number,
+      reqired: true,
+      default: 0,
+    },
+
+    image: {
+      type: String,
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
