4c2a73432eaaff7ae34fe2553ddcc12f31e50953
aghp81 <aghp81@gmail.com>
2024-06-07 10:47:30 +0330

    اتصال به دیتابیس mongoDB
    اتصال به دیتابیس mongoDB

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // console.log("hello world");
 
 const express = require('express')
+const mongoose = require('mongoose');
 const app = express()
 
 app.listen(3000, ()=>{
@@ -11,3 +12,11 @@ app.listen(3000, ()=>{
 app.get('/', (req, res)=> {
     res.send("Hello from node API Server");
 });
+
+mongoose.connect("mongodb+srv://aghp81:DAHHNyAufi9OWwLv@backenddb.qogzrsv.mongodb.net/node-crud-API?retryWrites=true&w=majority&appName=backendDB").then(() => {
+    console.log("به دیتابیس وصل شدید Success")
+})
+.catch(() => {
+    console.log("خطا در اتصال به دیتابیس failed")
+
+})
