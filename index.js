4d3ba7029e0bbbefc92a281827f4e30aa03c34e2
aghp81 <aghp81@gmail.com>
2024-06-07 10:50:50 +0330

    Databease Connected, Serve is running on port 3000

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const express = require('express')
 const mongoose = require('mongoose');
 const app = express()
 
-app.listen(3000, ()=>{
-    console.log('server is running on port 3000');
-});
 
 
 app.get('/', (req, res)=> {
@@ -14,7 +11,10 @@ app.get('/', (req, res)=> {
 });
 
 mongoose.connect("mongodb+srv://aghp81:DAHHNyAufi9OWwLv@backenddb.qogzrsv.mongodb.net/node-crud-API?retryWrites=true&w=majority&appName=backendDB").then(() => {
-    console.log("به دیتابیس وصل شدید Success")
+    console.log("به دیتابیس وصل شدید Success");
+    app.listen(3000, ()=>{
+        console.log('server is running on port 3000');
+    });
 })
 .catch(() => {
     console.log("خطا در اتصال به دیتابیس failed")
