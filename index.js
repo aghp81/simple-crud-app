72b526f918c92f08a774d23a5b2dd8205cda001e
aghp81 <aghp81@gmail.com>
2024-06-07 14:48:19 +0330

    ویرایش یک محصول

--- index.js
+++ index.js
@@ -43,6 +43,24 @@ app.post("/api/products", async (req, res) => {
   }
 });
 
+// ویرایش یک محصول
+app.put("/api/product/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body);
+
+        if(!product) {
+            return res.status(404).json({message: "محصول وجود ندارد"});
+        }
+
+        const updatedProduct = await Product.findById(id);
+        res.status(200).json(updatedProduct);
+
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  });
+
 mongoose
   .connect(
     "mongodb+srv://aghp81:DAHHNyAufi9OWwLv@backenddb.qogzrsv.mongodb.net/node-crud-API?retryWrites=true&w=majority&appName=backendDB"
