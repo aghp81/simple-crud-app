3f00fc4e83f27b35eb67a8b3b04630f4d391e9cc
aghp81 <aghp81@gmail.com>
2024-06-07 11:40:13 +0330

    app.use(express.json());

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
 // console.log("hello world");
 
-const express = require('express')
-const mongoose = require('mongoose');
-const app = express()
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
 
+app.use(express.json());
 
-
-app.get('/', (req, res)=> {
-    res.send("Hello from node API Server");
+app.get("/", (req, res) => {
+  res.send("Hello from node API Server");
 });
 
-app.post('/api/products', (req, res) => {
-    console.log(req.body);
-    res.send(req.body);
+app.post("/api/products", (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
 });
 
-mongoose.connect("mongodb+srv://aghp81:DAHHNyAufi9OWwLv@backenddb.qogzrsv.mongodb.net/node-crud-API?retryWrites=true&w=majority&appName=backendDB").then(() => {
+mongoose
+  .connect(
+    "mongodb+srv://aghp81:DAHHNyAufi9OWwLv@backenddb.qogzrsv.mongodb.net/node-crud-API?retryWrites=true&w=majority&appName=backendDB"
+  )
+  .then(() => {
     console.log("به دیتابیس وصل شدید Success");
-    app.listen(3000, ()=>{
-        console.log('server is running on port 3000');
+    app.listen(3000, () => {
+      console.log("server is running on port 3000");
     });
-})
-.catch(() => {
-    console.log("خطا در اتصال به دیتابیس failed")
-
-})
+  })
+  .catch(() => {
+    console.log("خطا در اتصال به دیتابیس failed");
+  });
